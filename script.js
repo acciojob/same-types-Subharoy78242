bd896dfa0e385079336f1ed147eb58b0b024060d
Subharoy78242 <57645302+Subharoy78242@users.noreply.github.com>
2024-05-20 15:22:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function isSameType(value1, value2) {
-  if ((Number.isNaN(value1) && Number.isNaN(value2)) || (value1 !== value1 && value2 !== value2)(Number.isNaN(value1) && Number.isNaN(value2)) || (value1 !== value1 && value2 !== value2)) {
+  if ((Number.isNaN(value1) && Number.isNaN(value2)) || (value1 !== value1 && value2 !== value2)) {
     return true;
   }
   return typeof value1 === typeof value2;
@@ -10,3 +10,20 @@ function isSameType(value1, value2) {
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 alert(isSameType(value1, value2));
+/*It seems like you are trying to check if two values are of the same type. Your function is mostly correct, but there is an issue with the handling of NaN.
+
+In JavaScript, NaN is considered a number type. So, when you check typeof NaN, it returns ‘number’. However, NaN is not equal to anything, even itself. So, NaN === NaN will return false.
+
+Your function isSameType(value1, value2) is checking isNaN(value1) && isNaN(value2) which will return true if both values are NaN. But the issue is that isNaN() will also return true for non-numeric strings and undefined values, which is not the desired behavior for this function.
+
+A better way to check if a value is NaN is to use Number.isNaN() function or value !== value (since NaN is the only JavaScript value that is treated as unequal to itself).
+
+Here is the corrected code:
+
+function isSameType(value1, value2) {
+  if ((Number.isNaN(value1) && Number.isNaN(value2)) || (value1 !== value1 && value2 !== value2)) {
+    return true;
+  }
+  return typeof value1 === typeof value2;
+}
+This will correctly handle NaN values and return true if both values are NaN, and false otherwise. It will also return true if both values are of the same type, and false otherwise.*/
